eb90cda4532b0bac60848a7e216d322fe5e0cac9
“Mars-Droidgit config --global user.name “Mars-Droid <c5321@mail.ru>
2020-12-04 15:17:59 +0500

    Initial Commit

---
+++ CookB WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  CookB WatchKit Extension
+//
+//  Created by Mars-Droid on 04.12.2020.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
